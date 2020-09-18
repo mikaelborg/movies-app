b195bf908e7b52e8990d3f8c4316dddab74901fb
Mika <mikaelborgc@gmail.com>
2020-09-18 22:20:38 +0200

    Updates

---
+++ precache-manifest.90fc84a699faa05cf935e4cce392eb66.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1116b950fdfd3850c2b5afe8612f54b2",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "d056bbabe8d7ed823699",
+    "url": "/movies-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7243487dca78b0e674bf",
+    "url": "/movies-app/static/css/main.7474c87e.chunk.css"
+  },
+  {
+    "revision": "d056bbabe8d7ed823699",
+    "url": "/movies-app/static/js/2.1465c183.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movies-app/static/js/2.1465c183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7243487dca78b0e674bf",
+    "url": "/movies-app/static/js/main.51fe86d1.chunk.js"
+  },
+  {
+    "revision": "0414ca83484ead7ccf72",
+    "url": "/movies-app/static/js/runtime-main.dbc8c99c.js"
+  },
+  {
+    "revision": "b9ac95d07455159c02abef2fc3fee64f",
+    "url": "/movies-app/static/media/Thumbs.b9ac95d0.db"
+  },
+  {
+    "revision": "398d9b0b748571fd2c0ec726eacc6d6e",
+    "url": "/movies-app/static/media/american-gangster.398d9b0b.jpg"
+  },
+  {
+    "revision": "011d76503d217bc6f0946ee6afd55496",
+    "url": "/movies-app/static/media/ant-man.011d7650.jpg"
+  },
+  {
+    "revision": "b56e7c65ff95d6d25b32ab3d383d04ee",
+    "url": "/movies-app/static/media/bad-boys.b56e7c65.jpg"
+  },
+  {
+    "revision": "3ff2e5b545ea3cd19c60858a14ea8ef0",
+    "url": "/movies-app/static/media/bridge-of-spies.3ff2e5b5.jpg"
+  },
+  {
+    "revision": "fe9e16af1c0aa1d02263a9c53e1cf0c9",
+    "url": "/movies-app/static/media/deadpool.fe9e16af.jpg"
+  },
+  {
+    "revision": "677ddf17baef067ecbfd12beded2310c",
+    "url": "/movies-app/static/media/fast-and-furious-7.677ddf17.jpg"
+  },
+  {
+    "revision": "9151c1936fa9da8f844272b8f7137838",
+    "url": "/movies-app/static/media/gangster-squad.9151c193.jpg"
+  },
+  {
+    "revision": "5ad54e33acdbe810aa77d2cc242c3a02",
+    "url": "/movies-app/static/media/gridiron-gang.5ad54e33.jpg"
+  },
+  {
+    "revision": "ac7178e3f3a78f93e704009f0fbdaad7",
+    "url": "/movies-app/static/media/jurassic-world.ac7178e3.jpg"
+  },
+  {
+    "revision": "193d269480ae2c9db0cdece371c6a48f",
+    "url": "/movies-app/static/media/legend.193d2694.jpg"
+  },
+  {
+    "revision": "9dc5cccb1b64e5d822069f2e071a4981",
+    "url": "/movies-app/static/media/mission-impossible-rogue-nation.9dc5cccb.jpg"
+  },
+  {
+    "revision": "30969f658775616bf210975bb0912c25",
+    "url": "/movies-app/static/media/now-you-see-me.30969f65.jpg"
+  },
+  {
+    "revision": "d56e8be7ad0461f41ea7b536ec34f01c",
+    "url": "/movies-app/static/media/project-x.d56e8be7.jpg"
+  },
+  {
+    "revision": "37b2f53e47c57841aaf8e74dd437e6f7",
+    "url": "/movies-app/static/media/running-scared.37b2f53e.jpg"
+  },
+  {
+    "revision": "c89d9d15a6892b98854ae6d34f624d41",
+    "url": "/movies-app/static/media/sicario.c89d9d15.jpg"
+  },
+  {
+    "revision": "16d30748c32b0a84a5cca47ce74cfd2b",
+    "url": "/movies-app/static/media/southpaw.16d30748.jpg"
+  },
+  {
+    "revision": "70d41515a877ecf1b17d6eee12593b35",
+    "url": "/movies-app/static/media/spectre.70d41515.jpg"
+  },
+  {
+    "revision": "bbfb60ae3e79ed37e38a563bb2d6d6ff",
+    "url": "/movies-app/static/media/straight-outta-compton.bbfb60ae.jpg"
+  },
+  {
+    "revision": "38b11925313eb035aa85f9b9a3236bf0",
+    "url": "/movies-app/static/media/the-dark-knight.38b11925.jpg"
+  },
+  {
+    "revision": "38bf85bece0930ad624ec0086e9c3b5c",
+    "url": "/movies-app/static/media/the-hangover.38bf85be.jpg"
+  },
+  {
+    "revision": "861dbbfd8103225c085aa1e1c91da725",
+    "url": "/movies-app/static/media/the-man-from-uncle.861dbbfd.jpg"
+  },
+  {
+    "revision": "49e5f493264a1a3132e2513ac2765472",
+    "url": "/movies-app/static/media/the-matrix.49e5f493.jpg"
+  },
+  {
+    "revision": "fef4f99d8756abcb21a916d4614e4f36",
+    "url": "/movies-app/static/media/tracers.fef4f99d.jpg"
+  },
+  {
+    "revision": "37c7e1a2b17cdea122f8db49a24ec9e1",
+    "url": "/movies-app/static/media/we-are-the-millers.37c7e1a2.jpg"
+  }
+]);
