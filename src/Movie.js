256354cc388830c669cb8c1e2ed0e4f69a619229
Mika <mikaelborgc@gmail.com>
2020-09-18 22:46:24 +0200

    root fix

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Movie = ({path, name, img}) => {
     return (
-        <Link className={style.movie} to={`/${path}`}>
+        <Link className={style.movie} to={`/movies-app/${path}`}>
             <h1 className={style.title}>{name}</h1>
             <img className={style.img} src={require(`./images/${img}`)} alt={img}/>
         </Link>
